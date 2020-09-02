c0af116d6feaa46fcca98f4aca2bbe16335123e3
“Rahul <rahulgupta6542@gmail.com>
2020-09-02 23:09:57 +0530

    rahul update initial

--- src/db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1/event-management', {
-	useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-})
